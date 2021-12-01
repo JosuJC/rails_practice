4d0edcfd820d98bab2dfa2fd060a7172cf800ff5
Josué Castillo <javicast07@gmail.com>
2021-12-01 14:39:32 -0600

    create an article within the page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_19_223118) do
+ActiveRecord::Schema.define(version: 2021_12_01_201223) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
